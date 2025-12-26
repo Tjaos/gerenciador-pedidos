e4a1db9607bb0a44b02197f494d826532181f872
Tjaos <th83287716@gmail.com>
2025-12-26 14:21:13 -0300

    feat: Create the Produto class

---
+++ src/main/java/com/thiagodev/gerenciador_pedidos/model/Produto.java
@@ -0,0 +1,51 @@
+package com.thiagodev.gerenciador_pedidos.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @Column(name = "valor")
+    private Double preco;
+
+    public Produto(Long id, String nome, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
